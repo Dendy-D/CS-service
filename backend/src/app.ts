328f501730d9bf5f0e52433390a03d04b27c29a4
daniil_vasilenko <daniilvasilenko2@mail.ru>
2021-11-28 03:24:30 +0300

    test commit4

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,3 +1,13 @@
 const a = 1;
 const b = 2     
 const c = 3
+const y = 9 
+
+// "@typescript-eslint/no-unused-vars": "warn",
+//     "@typescript-eslint/ban-types": "warn",
+//     "@typescript-eslint/no-use-before-define": "warn",
+//     "@typescript-eslint/no-shadow": "error",
+//     "@typescript-eslint/no-useless-constructor": "error",
+//     "@typescript-eslint/no-var-requires": "off",
+//     "@typescript-eslint/no-unused-expressions": "warn",
+//     "@typescript-eslint/type-annotation-spacing": "error"
