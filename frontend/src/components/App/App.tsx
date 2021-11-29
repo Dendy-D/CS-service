c7c4320ac23d20063008b6de7627bbd8e57f12d0
daniil_vasilenko <daniilvasilenko2@mail.ru>
2021-11-29 03:25:57 +0300

    add  linter on frontend

--- frontend/src/components/App/App.tsx
+++ frontend/src/components/App/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import './App.css';
+import classes from './App.module.scss';
 
 const App: React.FC = () => {
+  let a = 0
   return (
-    <div className="App">
+    <div className={classes.component}>
       <h1>hELLO</h1>
     </div>
   );
