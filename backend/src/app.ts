6a41e9594ce7d2ea30975419cc3dab8d10293ac4
daniil_vasilenko <daniilvasilenko2@gmail.com>
2022-01-24 23:46:10 +0300

    [refactor]: app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,21 +3,21 @@ import config from 'config';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 
+import authRoutes from './modules/auth/routes';
 import { sequelize } from './database/db';
 import Admin from './models/Admin';
 
-const app = express();
-
-app.use(cors());
-app.use(bodyParser.json({ limit: '50mb' }));
-app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
-
-// app.use('/auth', authRoutes());
-
-const PORT = config.get('PORT') || 5001;
-
-async function start() {
+const start = async () => {
   try {
+    const app = express();
+    const PORT = config.get('PORT') || 5001;
+
+    app.use(cors());
+    app.use(bodyParser.json({ limit: '50mb' }));
+    app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+
+    app.use('/auth', authRoutes());
+
     app.get('/', (req, res) => {
       res.send('<h1>Server is runnig</h1>');
     });
@@ -36,12 +36,13 @@ async function start() {
     app.listen(PORT, () => {
       console.log(`Server is runnig on: http://localhost:${PORT}`);
     });
+
     //TODO: should fix "any" tipe
   } catch (e: any) {
     console.log('Server Error', e.message);
     process.exit(1);
   }
-}
+};
 
 start();
 
