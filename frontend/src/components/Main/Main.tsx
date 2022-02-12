9ba175ca44ebd6b00cd038a2f15c0d0070b59335
daniil_vasilenko <daniilvasilenko2@gmail.com>
2022-02-12 14:16:02 +0300

    feat: Add Main component shell

--- frontend/src/components/Main/Main.tsx
+++ frontend/src/components/Main/Main.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 
+import classes from './Main.module.scss';
+
 const Main: React.FC = () => (
-  <div>
+  <div className={classes.component}>
     <h1>Main</h1>
   </div>
 );
