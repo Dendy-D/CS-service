5718f321a8ed8d2cde0860044ef619696952a756
daniil_vasilenko <daniilvasilenko2@gmail.com>
2022-02-13 02:53:49 +0300

    feat: Add structure from Menu component

---
+++ frontend/src/components/Menu/Menu.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import classes from './Menu.module.scss';
+
+const Menu = () => {
+  return (
+    <div className={classes.component}>
+      <h1>Menu</h1>
+    </div>
+  );
+};
+
+export default Menu;
