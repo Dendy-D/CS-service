261012dd601318289f69310f88f293aadb53435f
daniil_vasilenko <daniilvasilenko2@gmail.com>
2022-02-12 17:45:25 +0300

    feat: Add Card component

---
+++ frontend/src/components/Card/Card.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import classes from './Card.module.scss';
+
+const Card: React.FC = () => {
+  return (
+    <div className={classes.component}>
+      <h1>a</h1>
+    </div>
+  );
+};
+
+export default Card;
