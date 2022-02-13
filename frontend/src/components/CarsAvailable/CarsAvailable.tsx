4f00bbe8a45e73bed5a2d1564989a5fee7613186
daniil_vasilenko <daniilvasilenko2@gmail.com>
2022-02-14 01:55:55 +0300

    feat: Add CarsAvailable component

--- frontend/src/components/CarsAvailable/CarsAvailable.tsx
+++ frontend/src/components/CarsAvailable/CarsAvailable.tsx
@@ -1,13 +1,27 @@
 import React from 'react';
+import uniqueId from 'lodash/uniqueId';
 
+import Card from '../Card';
+import carsDataBase from '../../models/carsDB';
 import classes from './CarsAvailable.module.scss';
 
-const CarsAvailable: React.FC = () => {
-  return (
-    <div className={classes.component}>
-      alalal
-    </div>
-  );
-};
+const CarsAvailable: React.FC = () => (
+  <div className={classes.component}>
+    {carsDataBase.map((car, index) => (
+      <Card
+        brand={car.brand}
+        model={car.model}
+        complectation={car.complectation}
+        color={car.color}
+        year={car.year}
+        price={car.price}
+        enginePower={car.enginePower}
+        engineVolume={car.engineVolume}
+        preview={car.preview}
+        key={uniqueId(`car_${index}`)}
+      />
+    ))}
+  </div>
+);
 
 export default CarsAvailable;
