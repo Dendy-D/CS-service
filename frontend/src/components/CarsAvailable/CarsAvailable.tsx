2183524689ca05f729a56b3c1537bed3e3efa513
daniil_vasilenko <daniilvasilenko2@gmail.com>
2022-02-13 20:37:08 +0300

    feat: Add sample CarsAvailable component

---
+++ frontend/src/components/CarsAvailable/CarsAvailable.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import classes from './CarsAvailable.module.scss';
+import Card from '../Card';
+
+const CarsAvailable: React.FC = () => {
+  return (
+    <div className={classes.component}>
+      alalal
+    </div>
+  );
+};
+
+export default CarsAvailable;
