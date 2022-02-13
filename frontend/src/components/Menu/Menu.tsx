5fd0141022d10692ec02067b5d4871c7d3389d20
daniil_vasilenko <daniilvasilenko2@gmail.com>
2022-02-13 17:53:54 +0300

    feat: Add Menu component

--- frontend/src/components/Menu/Menu.tsx
+++ frontend/src/components/Menu/Menu.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import classes from './Menu.module.scss';
 
-const Menu = () => {
+const Menu: React.FC = () => {
   return (
     <div className={classes.component}>
-      <h1>Menu</h1>
+      <Link to="/employees">Сотрудники</Link>
+      <Link to="/clients">Клиенты</Link>
+      <Link to="/models">Модельный ряд</Link>
+      <Link to="/cars-available">Автомобили в наличии</Link>
+      <Link to="/test-drive">Запись на тест драйв</Link>
+      <Link to="/deals-made">Заключенные сделки</Link>
+      <Link to="/lending">Кредитование и лизинг</Link>
+      <Link to="/contract-of-sale">Договор купли-продажи</Link>
     </div>
   );
 };
