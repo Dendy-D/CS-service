022b31245f52dd8c30fd123e8e076a4257644df4
daniil_vasilenko <daniilvasilenko2@gmail.com>
2022-02-14 01:53:59 +0300

    fix: Styles Card component

--- frontend/src/components/Card/Card.tsx
+++ frontend/src/components/Card/Card.tsx
@@ -2,51 +2,43 @@ import React from 'react';
 import clsx from 'clsx';
 
 import { priceUnifier } from '../../utils/priceUnifier';
+import { Car } from '../../types/Car';
 import classes from './Card.module.scss';
 
-type CardProps = {
-  brand?: string;
-  model?: string;
-  complectation?: string;
-  color?: 'black' | 'white' | 'grey';
-  year?: number;
-  price?: number;
-  preview?: string;
-};
+type CardProps = Car
 
-const URL = 'https://i.ibb.co/6N45SYR';
+const Card: React.FC<CardProps> = (props: Car) => {
 
-const l = {
-  brand: 'Mercedes',
-  model: 'Mercedes-Benz',
-  complectation: 'GLS-Class',
-  color: 'black',
-  year: 2022,
-  price: 10890000,
-  preview: `${URL}/mers.png`,
-};
+  const {
+    brand,
+    model,
+    complectation,
+    color,
+    year,
+    price,
+    enginePower,
+    engineVolume,
+    preview,
+  } = props;
 
-const Card: React.FC<CardProps> = (props) => {
   const colorClasses = clsx({
     [classes.color]: true,
-    [classes[l.color]]: !!l.color,
+    [classes[color]]: !!color,
   });
-
-  const price = priceUnifier(l.price);
 
   return (
     <div className={classes.component}>
       <div className={classes.cardHeader}>
-        <span className={classes.model}>{l.model}</span>
+        <span className={classes.model}>{brand}</span>
       </div>
-      <div className={classes.complectation}>{l.complectation}</div>
-      <img src={l.preview} alt="car" className={classes.preview} />
+      <div className={classes.complectation}>{model}</div>
+      <img src={preview} alt="car" className={classes.preview} />
       <div className={classes.cardBottom}>
         <div className={classes.colors}>
           <span>Цвета:</span>
           <span className={colorClasses} />
         </div>
-        {price}
+        {priceUnifier(price)}
       </div>
     </div>
   );
