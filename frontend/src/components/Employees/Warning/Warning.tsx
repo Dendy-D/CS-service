969f4c81a8a8aa501e48cf808e85f6b39432ff21
daniil_vasilenko <daniilvasilenko2@gmail.com>
2022-02-22 06:10:09 +0300

    feat(Warning): Change remove by dismissal

--- frontend/src/components/Employees/Warning/Warning.tsx
+++ frontend/src/components/Employees/Warning/Warning.tsx
@@ -21,21 +21,21 @@ const Warning: React.FC<WarningProps> = (props: WarningProps) => {
     closeWarning();
   };
 
-  const onRemove = () => {
-    EmployeeStore.removeEmployee(employeeId);
+  const dismissalHandle = () => {
+    EmployeeStore.dismissalEmployee(employeeId);
     closeWarning();
   };
 
   return (
     <div className={classes.component}>
       <div className={classes.window}>
-        <div className={classes.question}>Вы уверены что хотите удалить сотрудника:
+        <div className={classes.question}>Вы уверены что хотите уволить сотрудника:
           <div>{employeeName}?</div>
         </div>
         <div className={classes.littleWarning}>[Это действие необратимо]</div>
         <div className={classes.buttons}>
           <button className={classes.cancel} onClick={onCancel}>Отмена</button>
-          <button className={classes.delete} onClick={onRemove}>Удалить</button>
+          <button className={classes.delete} onClick={dismissalHandle}>Уволить</button>
         </div>
       </div>
     </div>
