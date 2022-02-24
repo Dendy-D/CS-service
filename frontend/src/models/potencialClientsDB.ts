e3a7c1c773cb938eed2ac244c99ce920c76d69e3
daniil_vasilenko <daniilvasilenko2@gmail.com>
2022-02-24 05:09:47 +0300

    feat(potencialClientsDB): add model

---
+++ frontend/src/models/potencialClientsDB.ts
@@ -0,0 +1,77 @@
+import { PotencialClient } from '../types/PotencialClient';
+
+const potencialClientsDataBase: Array<PotencialClient> = [
+  {
+    id: 'skj6f3bas83',
+    fullName: 'Васнецов Николай Иванович',
+    phoneNumber: '8-956-345-20-98',
+    potencialCarId: '12fd850svg6',
+    leasing: false,
+  },
+  {
+    id: '9hj6f3vs583',
+    fullName: 'Калинин Виктор Степанович',
+    phoneNumber: '8-955-389-10-10',
+    potencialCarId: '30f7s648f6g',
+    leasing: true,
+  },
+  {
+    id: '0ijvsdb3483',
+    fullName: 'Закидыш Нарек Тимурович',
+    phoneNumber: '8-956-435-10-98',
+    potencialCarId: '345dfc2bsr4',
+    leasing: true,
+  },
+  {
+    id: '1ds6f35bc87',
+    fullName: 'Курягин Василий Денисович',
+    phoneNumber: '8-959-345-00-87',
+    potencialCarId: '35dlthjvb4n',
+    leasing: false,
+  },
+  {
+    id: '2fdv740gns6',
+    fullName: 'Малышов Антон Кирилович',
+    phoneNumber: '8-959-777-07-98',
+    potencialCarId: '354gsv9g5hs',
+    leasing: true,
+  },
+  {
+    id: 's54gfcx20j6',
+    fullName: 'Конь Константин Антонович',
+    phoneNumber: '8-999-345-99-99',
+    potencialCarId: '36dfv96hsnx',
+    leasing: false,
+  },
+  {
+    id: 'dfb5j4s96ng',
+    fullName: 'Ковалев Артем Артемович',
+    phoneNumber: '8-976-223-28-06',
+    potencialCarId: '7568hdxbs43',
+    leasing: false,
+  },
+  {
+    id: '5gdn04na6sg',
+    fullName: 'Птаха Николай Викторович',
+    phoneNumber: '8-955-777-90-08',
+    potencialCarId: '69ghns54vs2',
+    leasing: true,
+  },
+  {
+    id: '3fsnxb5lgyn',
+    fullName: 'Прокопенко Иван Иванович',
+    phoneNumber: '8-956-212-25-78',
+    potencialCarId: '30f7s648f6g',
+    leasing: false,
+  },
+  {
+    id: '5gsnxf30jtn',
+    fullName: 'Василюк Мария Иванова',
+    phoneNumber: '8-956-895-25-35',
+    potencialCarId: '12fd850svg6',
+    leasing: true,
+  },
+
+];
+
+export default potencialClientsDataBase;
