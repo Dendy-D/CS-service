ba46e2bf6c42411a39900ee43dc967e902727af8
daniil_vasilenko <daniilvasilenko2@gmail.com>
2022-02-24 05:08:23 +0300

    feat(ClientBoughtCar): add type

---
+++ frontend/src/types/ClientBoughtCar.ts
@@ -0,0 +1,11 @@
+export type ClientBoughtCar = {
+  id: string;
+  fullName: string;
+  phoneNumber: string;
+  carId: string;
+  contractOfSaleId: string;
+  dateOfBirst: string;
+  sex: 'male' | 'female';
+  placeOfBirth: string;
+  seriesAndNumbers: string;
+};
