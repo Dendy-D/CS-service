51f784dd50a1429aef4600457de729e02edc6be4
daniil_vasilenko <daniilvasilenko2@gmail.com>
2022-02-25 02:13:17 +0300

    feat(testDriveEntryes): add model

---
+++ frontend/src/models/testDriveEntryes.ts
@@ -0,0 +1,69 @@
+import { TestDriveEntry } from '../types/TestDriveEntry';
+
+const testDriveEntryesDataBase: Array<TestDriveEntry> = [
+  {
+    id: 'sf37fgs4n5b',
+    fullNameClient: 'Васнецов Николай Иванович',
+    carId: '12fd850svg6',
+    fullNameEmployee: 'Колчаков Максим Олегович',
+    date: '20.03.2022 19:00',
+  },
+  {
+    id: 'ad3hcb56d43',
+    fullNameClient: 'Калинин Виктор Степанович',
+    carId: '30f7s648f6g',
+    fullNameEmployee: 'Колчаков Максим Олегович',
+    date: '23.05.2022 12:00',
+  },
+  {
+    id: '46df3c29dgb',
+    fullNameClient: 'Закидыш Нарек Тимурович',
+    carId: '69ghns54vs2',
+    fullNameEmployee: 'Колчаков Максим Олегович',
+    date: '01.04.2022 14:30',
+  },
+  {
+    id: 'df30sgdb64j',
+    fullNameClient: 'Малышов Антон Кирилович',
+    carId: '12fd850svg6',
+    fullNameEmployee: 'Манукян Нарек Каренович',
+    date: '17.05.2022 13:15',
+  },
+  {
+    id: 's54fv3n7dk0',
+    fullNameClient: 'Конь Константин Антонович',
+    carId: '7568hdxbs43',
+    fullNameEmployee: 'Манукян Нарек Каренович',
+    date: '02.04.2022 18:35',
+  },
+  {
+    id: 'er3856fg34sc',
+    fullNameClient: 'Ковалев Артем Артемович',
+    carId: '36dfv96hsnx',
+    fullNameEmployee: 'Колчаков Максим Олегович',
+    date: '15.03.2022 16:10',
+  },
+  {
+    id: 'fg3bc05t4gns',
+    fullNameClient: 'Птаха Николай Викторович',
+    carId: '95gsbxhj58h',
+    fullNameEmployee: 'Колчаков Максим Олегович',
+    date: '23.04.2022 19:30',
+  },
+  {
+    id: 's54gbfk9dy3',
+    fullNameClient: 'Прокопенко Иван Иванович',
+    carId: '345dfc2bsr4',
+    fullNameEmployee: 'Манукян Нарек Каренович',
+    date: '26.04.2022 09:00',
+  },
+  {
+    id: 'df39sbxf349',
+    fullNameClient: 'Василюк Мария Иванова',
+    carId: '354gsv9g5hs',
+    fullNameEmployee: 'Манукян Нарек Каренович',
+    date: '05.05.2022 17:05',
+  }
+];
+
+export default testDriveEntryesDataBase;
