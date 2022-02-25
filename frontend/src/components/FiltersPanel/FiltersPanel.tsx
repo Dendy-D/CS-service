b5455cc0e794f7fe868c0efd05faa0a5b6e726da
daniil_vasilenko <daniilvasilenko2@gmail.com>
2022-02-26 02:31:55 +0300

    feat(FiltersPanel): initialize component

---
+++ frontend/src/components/FiltersPanel/FiltersPanel.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import classes from './FiltersPanel.module.scss';
+
+const FiltersPanel = () => {
+  return (
+    <div className={classes.component}>
+      <h1>sjs</h1>
+    </div>
+  );
+};
+
+export default FiltersPanel;
