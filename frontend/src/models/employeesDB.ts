b6106e4496aaba804400ea4436abf449d1607596
daniil_vasilenko <daniilvasilenko2@gmail.com>
2022-02-26 13:33:09 +0300

    fix(employeesDB): fix problem

--- frontend/src/models/employeesDB.ts
+++ frontend/src/models/employeesDB.ts
@@ -2,7 +2,7 @@ const employeesDataBase = {
   admin: {
     id: '46g7dvbdndn',
     login: 'toni-stark',
-    password: 89143978701,
+    password: '89143978701',
     fullName: 'Амельченко Антон Олегович',
     email: 'anton-amelchenko@mail.ru',
     phoneNumber: '8-914-777-22-03',
@@ -13,7 +13,7 @@ const employeesDataBase = {
     {
       id: '46g7dvb3g54',
       login: 'nikIvanov',
-      password: 123321,
+      password: '123321',
       fullName: 'Иванов Николай Константинович',
       email: 'nikolay-ivanov@mail.ru',
       phoneNumber: '8-914-111-22-03',
@@ -23,7 +23,7 @@ const employeesDataBase = {
     {
       id: '3fg76k7dndn',
       login: 'egVas',
-      password: 123321,
+      password: '123321',
       fullName: 'Егоров Василий Петрович',
       email: 'vasiliy-egorov@mail.ru',
       phoneNumber: '8-322-453-23-94',
@@ -35,21 +35,21 @@ const employeesDataBase = {
     {
       id: '2fsc47fgndb',
       login: 'kolMax',
-      password: 123321,
+      password: '123321',
       fullName: 'Колчаков Максим Олегович',
       email: 'kolchakov-max@mail.ru',
       phoneNumber: '8-934-122-45-32',
-      position: 'Тест-драйвер',
+      position: 'Тест-Драйвер',
       status: 'working'
     },
     {
       id: 'af4789jhfm9',
       login: 'manukkNar',
-      password: 123321,
+      password: '123321',
       fullName: 'Манукян Нарек Каренович',
       email: 'narek-manuk@mail.ru',
       phoneNumber: '8-566-342-08-99',
-      position: 'Тест-драйвер',
+      position: 'Тест-Драйвер',
       status: 'working'
     },
   ],
@@ -57,7 +57,7 @@ const employeesDataBase = {
     {
       id: '09g7dvbfnl8',
       login: 'markTven',
-      password: 123321,
+      password: '123321',
       fullName: 'Астер Марк Фрейд',
       email: 'mark-carry@mail.ru',
       phoneNumber: '8-914-232-22-13',
@@ -67,7 +67,7 @@ const employeesDataBase = {
     {
       id: '84n2dvscxdn',
       login: 'dimasikemployeesriy_Shevchuk',
-      password: 123321,
+      password: '123321',
       fullName: 'Шевчук Дмитрий Николаевич',
       email: 'dmitriy-shevchukv@mail.ru',
       phoneNumber: '8-111-223-56-94',
@@ -79,7 +79,7 @@ const employeesDataBase = {
     {
       id: '06h7dvbdsx4',
       login: 'mihmih',
-      password: 123321,
+      password: '123321',
       fullName: 'Братов Михали Юрьевич',
       email: 'misha-brat@mail.ru',
       phoneNumber: '8-800-231-22-13',
@@ -89,7 +89,7 @@ const employeesDataBase = {
     {
       id: '06mmdvbnm67',
       login: 'semich',
-      password: 123321,
+      password: '123321',
       fullName: 'Иванов Семен Степанович',
       email: 'semenov-semen@mail.ru',
       phoneNumber: '8-567-765-56-94',
@@ -101,7 +101,7 @@ const employeesDataBase = {
     {
       id: 'v3x7d90mn34v',
       login: 'kolyaKol',
-      password: 123321,
+      password: '123321',
       fullName: 'Градов Николай Алексеевич',
       email: 'nikola-tesla@mail.ru',
       phoneNumber: '8-999-777-66-55',
@@ -111,7 +111,7 @@ const employeesDataBase = {
     {
       id: '12bfl6sx54s',
       login: 'maric',
-      password: 123321,
+      password: '123321',
       fullName: 'Марисов Артур Маревич',
       email: 'maricov-artur@mail.ru',
       phoneNumber: '8-123-321-44-44',
@@ -123,7 +123,7 @@ const employeesDataBase = {
     {
       id: '23vd95gfns4',
       login: 'slavkal',
-      password: 123321,
+      password: '123321',
       fullName: 'Помидоров Вячеслав Александрович',
       email: 'pomidorich-slavyan@mail.ru',
       phoneNumber: '8-123-123-12-34',
@@ -133,7 +133,7 @@ const employeesDataBase = {
     {
       id: '5lfvx05683d',
       login: 'kirich',
-      password: 123321,
+      password: '123321',
       fullName: 'Антипов Кирил Васильевич',
       email: 'antipov-kiril@mail.ru',
       phoneNumber: '8-222-222-22-22',
@@ -145,7 +145,7 @@ const employeesDataBase = {
     {
       id: 'lkdg402s4f4',
       login: 'borisLoh',
-      password: 123321,
+      password: '123321',
       fullName: 'Гренов Борис Борисович',
       email: 'boris-povis@mail.ru',
       phoneNumber: '8-345-678-32-43',
@@ -155,7 +155,7 @@ const employeesDataBase = {
     {
       id: 'bsg405msh3j',
       login: 'antonioBanderas',
-      password: 123321,
+      password: '123321',
       fullName: 'Власов Антон Данилович',
       email: 'antoha-ahotna@mail.ru',
       phoneNumber: '8-955-324-58-68',
