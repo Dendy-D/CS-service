19bec45c02fe10e8b54ac4f2598268deca160c33
daniil_vasilenko <daniilvasilenko2@gmail.com>
2022-02-28 10:11:44 +0300

    feat(TestDrive): Update forms

--- frontend/src/components/TestDrive/TestDriveAddForm/TestDriveAddForm.tsx
+++ frontend/src/components/TestDrive/TestDriveAddForm/TestDriveAddForm.tsx
@@ -1,27 +1,21 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
+import { TestDriveEntryForForm } from '../../../types/TestDriveEntry';
 import TestDriveEntryesStore from '../../../stores/TestDriveEntryesStore';
 import classes from './TestDriveAddForm.module.scss';
-
-type Form = {
-  fullNameClient: string;
-  fullNameEmployee: string;
-  carId: string;
-  date: string;
-};
 
 const TestDriveAddForm: React.FC = () => {
   const navigate = useNavigate();
 
-  const [form, setForm] = useState<Form>({
+  const [form, setForm] = useState<TestDriveEntryForForm>({
     fullNameClient: '',
     fullNameEmployee: '',
     carId: '',
     date: '',
   });
 
-  const formChangeHandler = (key: keyof Form, value: string) => {
+  const formChangeHandler = (key: keyof TestDriveEntryForForm, value: string) => {
     setForm({ ...form, [key]: value });
   };
 
@@ -35,40 +29,44 @@ const TestDriveAddForm: React.FC = () => {
       <div className={classes.wrapper}>
         <h1 className={classes.title}>Добавление записи на тест-драйв</h1>
         <div>
-          <label htmlFor="fullNameClient">ФИО Клиента</label>
-          <input
-            name="fullNameClient"
-            type="text"
-            value={form.fullNameClient}
-            onChange={(e) => formChangeHandler('fullNameClient', e.target.value)}
-          />
+          <label htmlFor="fullNameClient">ФИО Клиента
+            <input
+              name="fullNameClient"
+              type="text"
+              value={form.fullNameClient}
+              onChange={(e) => formChangeHandler('fullNameClient', e.target.value)}
+            />
+          </label>
         </div>
         <div>
-          <label htmlFor="fullNameEmployee">ФИО Сотрудника</label>
-          <input
-            name="fullNameEmployee"
-            type="text"
-            value={form.fullNameEmployee}
-            onChange={(e) => formChangeHandler('fullNameEmployee', e.target.value)}
-          />
+          <label htmlFor="fullNameEmployee">ФИО Сотрудника
+            <input
+              name="fullNameEmployee"
+              type="text"
+              value={form.fullNameEmployee}
+              onChange={(e) => formChangeHandler('fullNameEmployee', e.target.value)}
+            />
+          </label>
         </div>
         <div>
-          <label htmlFor="carId">Автомобиль</label>
-          <input
-            name="carId"
-            type="text"
-            value={form.carId}
-            onChange={(e) => formChangeHandler('carId', e.target.value)}
-          />
+          <label htmlFor="carId">Автомобиль
+            <input
+              name="carId"
+              type="text"
+              value={form.carId}
+              onChange={(e) => formChangeHandler('carId', e.target.value)}
+            />
+          </label>
         </div>
         <div>
-          <label htmlFor="date">Дата</label>
-          <input
-            name="date"
-            type="text"
-            value={form.date}
-            onChange={(e) => formChangeHandler('date', e.target.value)}
-          />
+          <label htmlFor="date">Дата
+            <input
+              name="date"
+              type="text"
+              value={form.date}
+              onChange={(e) => formChangeHandler('date', e.target.value)}
+            />
+          </label>
         </div>
         <div>
           <button className={classes.editButton} onClick={addTestDriveEntry}>
