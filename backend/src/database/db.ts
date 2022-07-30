4f9df84313ef84a72a9c5aabfa299d065f12a639
daniil_vasilenko <daniilvasilenko2@gmail.com>
2022-07-31 02:57:57 +0300

    feat: db schema

--- backend/src/database/db.ts
+++ backend/src/database/db.ts
@@ -1,20 +1,11 @@
-import { Sequelize } from 'sequelize';
-import config from 'config';
+import { Pool } from 'pg';
 
-import { testConnectionToDataBase } from '../utils/db-helper';
-const DB_NAME: string = config.get('DB_NAME');
-const DB_USERNAME: string = config.get('DB_USERNAME');
-const DB_PASSWORD: string = config.get('DB_PASSWORD');
-const DB_HOST_NAME: string = config.get('DB_HOST_NAME');
-const DB_PORT_NAME: number = config.get('DB_PORT_NAME');
-
-export const sequelize = new Sequelize(DB_USERNAME, DB_NAME, DB_PASSWORD, {
-  host: DB_HOST_NAME,
-  dialect: 'postgres',
-  port: DB_PORT_NAME,
-  define: {
-    timestamps: false
-  }
+const pool = new Pool({
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST_NAME,
+  port: process.env.DB_PORT_NAME as number | undefined,
+  database: process.env.DB_NAME,
 });
 
-testConnectionToDataBase(sequelize);
+export default pool;
