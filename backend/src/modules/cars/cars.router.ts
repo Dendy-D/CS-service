d5d326a4dbcbd382bb59ddc059d16b1eba6f0ae2
Dendy-D <daniilvasilenko2@gmail.com>
2023-09-23 00:55:58 +0300

    feat(modules): add cars

---
+++ backend/src/modules/cars/cars.router.ts
@@ -0,0 +1,13 @@
+import { Router } from 'express';
+
+import { getCars, addCar, getCarById, updateCar, deleteCar } from './controller';
+
+const router = Router();
+
+router.get('/', getCars);
+router.post('/', addCar);
+router.put('/:id', updateCar);
+router.get('/:id', getCarById);
+router.delete('/:id', deleteCar);
+
+export default router;
