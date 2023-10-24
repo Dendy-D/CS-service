ee1ad0fbcfcd52aaf2ff4af5dc49d7ebb329502e
Dendy-D <daniilvasilenko2@gmail.com>
2023-10-24 21:49:24 +0300

    feat(components): modify App

--- frontend/src/components/App/App.tsx
+++ frontend/src/components/App/App.tsx
@@ -1,8 +1,22 @@
 import React from 'react';
 
+import Main from '../routes/Main';
+import Header from '../ui/Header';
+import Menu from '../Menu';
+import AppRouter from '../AppRouter';
+import classes from './App.module.scss';
+
 const App: React.FC = () => {
   return (
-    <div>Hello React</div>
+    <div className={classes.component}>
+      <div className={classes.sideBar}>
+        <Menu />
+      </div>
+      <div className={classes.content}>
+        <Header />
+        <AppRouter />
+      </div>
+    </div>
   );
 };
 
