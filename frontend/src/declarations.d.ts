063b83e3911abe7280a9add1643c623d2aba472d
Dendy-D <daniilvasilenko2@gmail.com>
2023-10-24 21:47:04 +0300

    feat(tsconfig): add new declaration for svg and modify tsconfig

---
+++ frontend/src/declarations.d.ts
@@ -0,0 +1,8 @@
+declare module '*.scss';
+
+declare module '*.svg' {
+  import React = require('react');
+  export const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
+  const content: string;
+  export default content;
+}
